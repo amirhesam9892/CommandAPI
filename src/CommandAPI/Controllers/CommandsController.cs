ca6057080bcc6b38c5227091f564f2740b76f149
Amirhesam Moradii <Moradii@fanni.tazarv.com>
2022-05-08 12:14:26 +0430

    Added Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.AddControllers
 {
@@ -8,11 +10,28 @@ namespace CommandAPI.AddControllers
         [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _repository = repository;
+        }
 
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-            return new string[] {"this","is","hard","code"};
+            //return new string[] {"this","is","hard","code"};
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if(commandItem == null )
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -17,6 +18,8 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
                 services.AddControllers();
+
+                services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
